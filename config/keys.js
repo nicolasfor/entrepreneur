7299d0b2226d66a184513ec0132adf44a84ca7f4
Nicolas Forero <nicolasfor@gmail.com>
2019-11-13 23:33:53 +0100

    First commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb+srv://admin:admin@cluster0-xioye.mongodb.net/test?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+var path = require('path');
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
+
+const app = express();
+
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+
+const public = path.join(__dirname, 'client/build/');
+const staticPath = path.join(__dirname, 'client/build/')
+
+app.use(express.static(staticPath))
+        .get('/', (req, res) => {
+            res.sendFile('index.html', {
+                root: staticPath
+            })
+        })
+app.use('/', express.static(public));     
+   
+app.use("/api/users", users);
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
